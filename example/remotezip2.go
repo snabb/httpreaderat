d7c0f71321e920f2783d8316b38587b11dfc6194
Janne Snabb <snabb@epipe.com>
2018-03-02 18:41:41 +0200

    Add buffering to example II.

--- example/remotezip2.go
+++ example/remotezip2.go
@@ -7,17 +7,19 @@ package main
 import (
 	"archive/zip"
 	"fmt"
+	"github.com/avvmoto/buf-readerat"
 	"github.com/snabb/htreaderat"
 )
 
 func main() {
 	htrdr := htreaderat.New(nil, "https://dl.google.com/go/go1.10.windows-amd64.zip", nil)
+	bhtrdr := bufra.NewBufReaderAt(htrdr, 1024*1024)
 
 	size, err := htrdr.Size()
 	if err != nil {
 		panic(err)
 	}
-	zrdr, err := zip.NewReader(htrdr, size)
+	zrdr, err := zip.NewReader(bhtrdr, size)
 	if err != nil {
 		panic(err)
 	}
