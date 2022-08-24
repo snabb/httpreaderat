e49db0b59b85d06de84f3d9021531eeccb558225
Guilhem Lettron <guilhem@barpilot.io>
2022-08-24 16:56:46 +0200

    chore: Add fixture tests

---
+++ fixtures/test1
@@ -0,0 +1,1 @@
+some line of text
--- httpreaderat_test.go
+++ httpreaderat_test.go
@@ -1,9 +1,12 @@
 package httpreaderat_test
 
 import (
+	"archive/zip"
 	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
+	"sync"
 	"testing"
 
 	"github.com/snabb/httpreaderat"
@@ -92,4 +95,58 @@ func (ra *readerAtFixture) TestRangeSupportIntialTooMuchResponse() {
 	reader, err := ra.reader()
 	ra.EqualError(err, "content-length mismatch in http response")
 	ra.Nil(reader)
+}
+
+func (ra *readerAtFixture) TestFile() {
+	ra.server = httptest.NewServer(http.FileServer(http.Dir("./fixtures")))
+
+	reader, err := ra.reader()
+	ra.Nil(err)
+	ra.NotNil(reader)
+
+	zfile, err := zip.NewReader(reader, reader.Size())
+	ra.NoError(err)
+	ra.Len(zfile.File, 4)
+
+	txtfile, err := zfile.Open("test2")
+	ra.NoError(err)
+	defer txtfile.Close()
+
+	content, err := io.ReadAll(txtfile)
+	ra.NoError(err)
+
+	ra.Equal(http.DetectContentType(content), "text/plain; charset=utf-8")
+}
+
+func (ra *readerAtFixture) TestParralelFiles() {
+	ra.server = httptest.NewServer(http.FileServer(http.Dir("./fixtures")))
+
+	reader, err := ra.reader()
+	ra.Nil(err)
+	ra.NotNil(reader)
+
+	zfile, err := zip.NewReader(reader, reader.Size())
+	ra.NoError(err)
+	ra.Len(zfile.File, 4)
+
+	var wg sync.WaitGroup
+
+	testread := func(f *zip.File) {
+		defer wg.Done()
+		file, err := f.Open()
+		ra.NoError(err)
+		defer file.Close()
+
+		content, err := io.ReadAll(file)
+		ra.NoError(err)
+		ra.NotEmpty(content, "name", f.Name)
+	}
+
+	for _, f := range zfile.File {
+		wg.Add(1)
+		go testread(f)
+	}
+
+	wg.Wait()
+
 }
